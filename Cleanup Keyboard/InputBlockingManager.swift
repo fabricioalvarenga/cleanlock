bb836145d5f35651d53cd91a4535890d2f356b50
Fabricio Alvarenga <fab.oliveira.alvarenga@gmail.com>
2025-05-04 19:27:39 -0300

    Create a callback method for both events (keyboard and trackpad)

--- Cleanup Keyboard/InputBlockingManager.swift
+++ Cleanup Keyboard/InputBlockingManager.swift
@@ -11,56 +11,60 @@ import Combine
 
 // Esta função de callback deve ficar fora do escopo da classe ou o seguinte erro aparecerá:
 // A C function pointer can only be formed from a reference to a 'func' or a literal closure
-func keyboardTapEventCallback(proxy: CGEventTapProxy,
+fileprivate func tapEventCallback(proxy: CGEventTapProxy,
                       type: CGEventType,
                       event: CGEvent,
                       refcon: UnsafeMutableRawPointer?) -> Unmanaged<CGEvent>? {
-    guard let refcon = refcon else {
+    guard let refcon else {
         return Unmanaged.passRetained(event)
     }
     
-    let monitor = Unmanaged<InputBlockingManager>.fromOpaque(refcon).takeUnretainedValue()
+    let eventInfo = Unmanaged<TapEventInfo>.fromOpaque(refcon).takeUnretainedValue()
+    let monitor = eventInfo.manager
     
-    if type == .flagsChanged {
-        let keyCode = event.getIntegerValueField(.keyboardEventKeycode)
-        
-        if keyCode == 56 || keyCode == 60 { // Teclas Shift (esquerda e direita)
-            let flags = event.flags
+    switch eventInfo.eventType {
+    case .keyboard:
+        if type == .flagsChanged {
+            let keyCode = event.getIntegerValueField(.keyboardEventKeycode)
             
-            switch keyCode {
-            case 56: monitor.isLeftShiftKeyPressed = flags.contains(.maskShift)
-            case 60: monitor.isRightShiftKeyPressed = flags.contains(.maskShift)
-            default: break
+            if keyCode == 56 || keyCode == 60 { // Teclas Shift (esquerda e direita)
+                let flags = event.flags
+                
+                switch keyCode {
+                case 56: monitor.isLeftShiftKeyPressed = flags.contains(.maskShift)
+                case 60: monitor.isRightShiftKeyPressed = flags.contains(.maskShift)
+                default: break
+                }
+                
+                return Unmanaged.passRetained(event)
             }
-            
+        }
+        
+        if !monitor.isKeyboardLocked {
             return Unmanaged.passRetained(event)
         }
-    }
-    
-    if !monitor.isKeyboardLocked {
-        return Unmanaged.passRetained(event)
+    case .trackpad:
+        if !monitor.isTrackpadLocked {
+            return Unmanaged.passRetained(event)
+        }
     }
     
     return nil
 }
 
-// Esta função de callback deve ficar fora do escopo da classe ou o seguinte erro aparecerá:
-// A C function pointer can only be formed from a reference to a 'func' or a literal closure
-func trackpadTapEventCallback(proxy: CGEventTapProxy,
-                      type: CGEventType,
-                      event: CGEvent,
-                      refcon: UnsafeMutableRawPointer?) -> Unmanaged<CGEvent>? {
-    guard let refcon = refcon else {
-        return Unmanaged.passRetained(event)
+fileprivate enum TapEventType {
+    case keyboard
+    case trackpad
+}
+
+fileprivate class TapEventInfo {
+    var manager: InputBlockingManager
+    var eventType: TapEventType
+    
+    init(manager: InputBlockingManager, eventType: TapEventType) {
+        self.manager = manager
+        self.eventType = eventType
     }
-    
-    let monitor = Unmanaged<InputBlockingManager>.fromOpaque(refcon).takeUnretainedValue()
-    
-    if !monitor.isTrackpadLocked {
-        return Unmanaged.passRetained(event)
-    }
-    
-    return nil
 }
 
 class InputBlockingManager: ObservableObject {
@@ -73,6 +77,8 @@ class InputBlockingManager: ObservableObject {
 
     private var keyboardTapLockEvent: CFMachPort?
     private var trackpadTapLockEvent: CFMachPort?
+    private var keyboardEventInfo: TapEventInfo?
+    private var trackpadEventInfo: TapEventInfo?
     private var shiftKeysTapEvent: CFMachPort?
     private var cancellables = Set<AnyCancellable>()
     
@@ -114,12 +120,16 @@ class InputBlockingManager: ObservableObject {
         CGEventMask(1 << CGEventType.keyUp.rawValue) |
         CGEventMask(1 << CGEventType.flagsChanged.rawValue)
         
+        // Cria estrutura de informações do evento para que o mesmo callback
+        // possa identificar se se trata de um evento de teclado ou trackpad
+        keyboardEventInfo = TapEventInfo(manager: self, eventType: .keyboard)
+        
         keyboardTapLockEvent = CGEvent.tapCreate(tap: .cgSessionEventTap,
                                                  place: .headInsertEventTap,
                                                  options: .defaultTap,
                                                  eventsOfInterest: eventMask,
-                                                 callback: keyboardTapEventCallback,
-                                                 userInfo: UnsafeMutableRawPointer(Unmanaged.passUnretained(self).toOpaque()))
+                                                 callback: tapEventCallback,
+                                                 userInfo: UnsafeMutableRawPointer(Unmanaged.passUnretained(keyboardEventInfo!).toOpaque()))
         
         guard let eventTap = keyboardTapLockEvent else { return }
         
@@ -133,7 +143,7 @@ class InputBlockingManager: ObservableObject {
                         CGEventMask(1 << CGEventType.rightMouseDown.rawValue) |
                         CGEventMask(1 << CGEventType.rightMouseUp.rawValue) |
                         CGEventMask(1 << CGEventType.rightMouseDragged.rawValue)
-
+        
         eventMask = eventMask |
                     CGEventMask(1 << CGEventType.mouseMoved.rawValue) |
                     CGEventMask(1 << CGEventType.otherMouseDown.rawValue) |
@@ -141,12 +151,16 @@ class InputBlockingManager: ObservableObject {
                     CGEventMask(1 << CGEventType.otherMouseDragged.rawValue) |
                     CGEventMask(1 << CGEventType.scrollWheel.rawValue)
 
+        // Cria estrutura de informações do evento para que o mesmo callback
+        // possa identificar se se trata de um evento de teclado ou trackpad
+        trackpadEventInfo = TapEventInfo(manager: self, eventType: .trackpad)
+        
         trackpadTapLockEvent = CGEvent.tapCreate(tap: .cghidEventTap,
                                                  place: .headInsertEventTap,
                                                  options: .defaultTap,
                                                  eventsOfInterest: eventMask,
-                                                 callback: trackpadTapEventCallback,
-                                                 userInfo: UnsafeMutableRawPointer(Unmanaged.passUnretained(self).toOpaque()))
+                                                 callback: tapEventCallback,
+                                                 userInfo: UnsafeMutableRawPointer(Unmanaged.passUnretained(trackpadEventInfo!).toOpaque()))
 
         guard let eventTap = trackpadTapLockEvent else { return }
         
