47922abe0fc25734531a0dcbb0efbfd044a50bde
Fabricio Alvarenga <fab.oliveira.alvarenga@gmail.com>
2025-05-13 21:16:52 -0300

    Improve keypress simulation

--- CleanLock/ViewModels/InputBlockingManager.swift
+++ CleanLock/ViewModels/InputBlockingManager.swift
@@ -21,19 +21,19 @@ fileprivate func tapEventCallback(proxy: CGEventTapProxy,
     
     let eventInfo = Unmanaged<TapEventInfo>.fromOpaque(refcon).takeUnretainedValue()
     let eventType = eventInfo.eventType
-    let monitor = eventInfo.manager
+    let manager = eventInfo.manager
     
     switch eventType {
     case .keyboard:
-        let keyCode = event.getDoubleValueField(.keyboardEventKeycode)
-        print(keyCode)
-        // Verifica se é um evento definido pelo sistema (systemDefined) para controle de mídia/volume
-//        if type == monitor.systemMidiaControlEventType {
-//            let data = event.getIntegerValueField(.eventSourceUnixProcessID)
+        // Verifica se é um evento definido pelo sistema (systemDefined)
+        // As teclas de brilho, volume e demais teclas de mídias são controladas por ese tipo de evento
+        if type == manager.systemDefinedEventType {
+//            let data = event.getIntegerValueField(CGEventField(rawValue: 45)!)
+            
 //            let keyFlags = Int32((data & 0xFFFF0000) >> 16)
 //            let keyData = (data & 0xFFFF)
 //            let keyState = (keyFlags & 0xFF00) >> 8
-//            
+            
 //            if ((keyData & 0xFF) == 7) || // Volume Up
 //                ((keyData & 0xFF) == 8) || // Volume Down
 //                ((keyData & 0xFF) == 3) { // Mute
@@ -42,28 +42,54 @@ fileprivate func tapEventCallback(proxy: CGEventTapProxy,
 //                let keyCode = keyData & 0xFF
 //                
 //                print("Tecla de Volume \(keyCode): \(keyDown == 1 ? "Pressionada" : "Liberada")")
-//                
-//                return nil
 //            }
-//        }
+        }
         
+        let keyCode = event.getIntegerValueField(.keyboardEventKeycode)
+        let flags = event.flags
+        
+        // Faz o tratamento de retorno para todas as teclas especiais
         if type == .flagsChanged {
-            let keyCode = event.getIntegerValueField(.keyboardEventKeycode)
-            let flags = event.flags
-            
             if keyCode == 56 || keyCode == 60 { // Teclas Shift (esquerda e direita)
                 switch keyCode {
-                case 56: monitor.isLeftShiftKeyPressed = flags.contains(.maskShift)
-                case 60: monitor.isRightShiftKeyPressed = flags.contains(.maskShift)
+                case 56: manager.isLeftShiftKeyPressed = flags.contains(.maskShift)
+                case 60: manager.isRightShiftKeyPressed = flags.contains(.maskShift)
                 default: break
                 }
+                
+                manager.setPressedKeyCodeValue(keyCode)
+                
                 return Unmanaged.passRetained(event)
+            }
+            else if (keyCode == 57 && flags.contains(.maskAlphaShift)) || // Tecla Caps Lock
+                        (keyCode == 63 && flags.contains(.maskSecondaryFn)) || // Tecla Fn
+                        (keyCode == 59 && flags.contains(.maskControl)) || // Tecla Control
+                        (keyCode == 58 && flags.contains(.maskAlternate)) || // Tecla Option (left)
+                        (keyCode == 61 && flags.contains(.maskAlternate)) || // Tecla Option (right)
+                        (keyCode == 55 && flags.contains(.maskCommand)) || // Tecla Command (left)
+                        (keyCode == 54 && flags.contains(.maskCommand)) { // Tecla Command (right)
+                manager.setPressedKeyCodeValue(keyCode)
             }
         }
         
-        if monitor.isKeyboardLocked { return nil }
+        // Se a tecla "Seta para Cima" for pressionada, retorna o código da tecla "Seta para Baixo",
+        // pois as duas estão desenhadas na mesma tecla no teclado virtual
+        // Do contrário, retorna o código de qualquer outra tecla que tenha sido pressionada
+        if type == .keyDown {
+            if keyCode == 126 {
+                manager.setPressedKeyCodeValue(125)
+            } else {
+                manager.setPressedKeyCodeValue(keyCode)
+            }
+        }
+        
+        if manager.isKeyboardLocked {
+            return nil
+        }
     case .trackpad:
-        if monitor.isTrackpadLocked { return nil }
+        if manager.isTrackpadLocked {
+            return nil
+        }
     }
     
     return Unmanaged.passRetained(event)
@@ -91,17 +117,56 @@ class InputBlockingManager: ObservableObject {
     @Published var isLeftShiftKeyPressed = false
     @Published var isRightShiftKeyPressed = false
     @Published var areBothShiftKeysPressed = false
+    @Published var pressedKeyCode: Int64?
 
     private var keyboardTapLockEvent: CFMachPort?
     private var trackpadTapLockEvent: CFMachPort?
+    private var keyboardRunLoopSource: CFRunLoopSource?
+    private var trackpadRunLoopSource: CFRunLoopSource?
     private var keyboardEventInfo: TapEventInfo?
     private var trackpadEventInfo: TapEventInfo?
-    private var shiftKeysTapEvent: CFMachPort?
     private var cancellables = Set<AnyCancellable>()
     
-    // Tipo 14 é para eventos definidos pelo sistema (systemDefined) - teclas especiais (volume, brilho, etc)
-    let systemMidiaControlEventType = CGEventType(rawValue: 14)!
+    // Tipo 14 é para eventos definidos pelo sistema (systemDefined) - teclas especiais (caps lock, volume, brilho, etc)
+    let systemDefinedEventType = CGEventType(rawValue: 14)!
+    
+    // Tamanhos relativos das teclas
+    let standardKeyWidth: CGFloat = 1.0
+    let specialKeyWidths: [String: CGFloat] = [
+        "esc": 1.6,
+        "delete": 1.6,
+        "tab": 1.6,
+        "caps": 1.875,
+        "return": 1.875,
+        "shift l": 2.45,
+        "shift r": 2.45,
+        "command l": 1.3,
+        "command r": 1.3,
+        "": 5.6,  // Barra de espaço
+    ]
+    
+    // Layout dos teclados ANSI EUA
+    let keyboard =
+    [
+        [(53, "esc"), (1001, "F1"), (1002, "F2"), (160, "F3"), (177, "F4"), (176, "F5"), (178, "F6"), (1003, "F7"),
+         (1004, "F8"), (1005, "F9"), (1006, "F10"), (1007, "F11"), (1008, "F12"), (1009, "on")],
         
+        [(50, "`"), (18, "1"), (19, "2"), (20, "3"), (21, "4"), (23, "5"), (22, "6"), (26, "7"),
+         (28, "8"), (25, "9"), (29, "0"), (27, "-"), (24, "="), (51, "delete")],
+        
+        [(48, "tab"), (12, "Q"), (13, "W"), (14, "E"), (15, "R"), (17, "T"), (16, "Y"),
+         (32, "U"), (34, "I"), (31, "O"), (35, "P"), (33, "["), (30, "]"), (42, "\\")],
+        
+        [(57, "caps"), (0, "A"), (1, "S"), (2, "D"), (3, "F"), (5, "G"), (4, "H"),
+         (38, "J"), (40, "K"), (37, "L"), (41, ";"), (39, "'"), (36, "return")],
+        
+        [(56, "shift l"), (6, "Z"), (7, "X"), (8, "C"), (9, "V"), (11, "B"), (45, "N"),
+         (46, "M"), (43, ","), (47, "."), (44, "/"), (60, "shift r")],
+        
+        [(63, "fn"), (59, "control"), (58, "option l"), (55, "command l"), (49, ""),
+         (54, "command r"), (61, "option r"), (123, "←"), (125, "↑↓"), (124, "→")],
+    ]
+    
     init() {
         Publishers.CombineLatest($isLeftShiftKeyPressed, $isRightShiftKeyPressed)
             .map { $0 && $1 }
@@ -120,15 +185,42 @@ class InputBlockingManager: ObservableObject {
         .store(in: &cancellables)
     }
     
-    deinit { isCleaning = false }
-   
-    func startCleaning() { isCleaning = true }
+    deinit {
+        isCleaning = false
+        
+        keyboardEventInfo = nil
+        keyboardTapLockEvent = nil
+        keyboardRunLoopSource = nil
+        
+        trackpadEventInfo = nil
+        trackpadTapLockEvent = nil
+        trackpadRunLoopSource = nil
+    }
     
-    func stopCleaning() { isCleaning = false }
+    func findKeyLabel(for keyCode: Int64) -> String? {
+        return keyboard.flatMap { $0 }.first { $0.0 == keyCode }?.1
+    }
+    
+    func setPressedKeyCodeValue(_ keyCode: Int64?) {
+        self.pressedKeyCode = keyCode
+    }
+   
+    func startCleaning() {
+        isCleaning = true
+    }
+    
+    func stopCleaning() {
+        isCleaning = false
+    }
     
     private func startMonitoring() {
         startKeyboardMonitoring()
         startTrackpadMonitoring()
+    }
+    
+    private func stopMonitoring() {
+        stopKeyboardMonitoring()
+        stopTrackpadMonitoring()
     }
    
     private func startKeyboardMonitoring() {
@@ -137,7 +229,7 @@ class InputBlockingManager: ObservableObject {
         CGEventMask(1 << CGEventType.flagsChanged.rawValue)
         
         // Eventos definidos pelo sistema (systemDefined) para teclas especiais (volume, brilho, etc)
-        eventMask = eventMask | (1 << systemMidiaControlEventType.rawValue)
+        eventMask = eventMask | (1 << systemDefinedEventType.rawValue)
         
         // Cria estrutura de informações do evento para que o mesmo callback
         // possa identificar se se trata de um evento de teclado ou trackpad
@@ -152,7 +244,9 @@ class InputBlockingManager: ObservableObject {
         
         guard let eventTap = keyboardTapLockEvent else { return }
         
-        enableTapEvent(eventTap)
+        keyboardRunLoopSource = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, eventTap, 0)
+        CFRunLoopAddSource(CFRunLoopGetCurrent(), keyboardRunLoopSource, .commonModes)
+        CGEvent.tapEnable(tap: eventTap, enable: true)
     }
     
     private func startTrackpadMonitoring() {
@@ -192,34 +286,37 @@ class InputBlockingManager: ObservableObject {
 
         guard let eventTap = trackpadTapLockEvent else { return }
         
-        enableTapEvent(eventTap)
-    }
-    
-    private func enableTapEvent(_ eventTap: CFMachPort) {
-        let runLoopSource = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, eventTap, 0)
-        CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource, .commonModes)
+        trackpadRunLoopSource = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, eventTap, 0)
+        CFRunLoopAddSource(CFRunLoopGetCurrent(), trackpadRunLoopSource, .commonModes)
         CGEvent.tapEnable(tap: eventTap, enable: true)
     }
     
-    private func stopMonitoring() {
-        stopKeyboardMonitoring()
-        stopTrackpadMonitoring()
-    }
-    
     private func stopKeyboardMonitoring() {
-        if let eventTap = keyboardTapLockEvent {
-            CGEvent.tapEnable(tap: eventTap, enable: false)
+        if let keyboardTapLockEvent {
+            CGEvent.tapEnable(tap: keyboardTapLockEvent, enable: false)
         }
+        
+        if let keyboardRunLoopSource {
+            CFRunLoopRemoveSource(CFRunLoopGetCurrent(), keyboardRunLoopSource, .commonModes)
+        }
+        
         keyboardTapLockEvent = nil
+        keyboardRunLoopSource = nil
     }
 
     private func stopTrackpadMonitoring() {
          // Mostra o ponteiro do trackpad/mouse
         CGDisplayShowCursor(CGMainDisplayID())
         
-        if let eventTap = trackpadTapLockEvent {
-            CGEvent.tapEnable(tap: eventTap, enable: false)
+        if let trackpadTapLockEvent {
+            CGEvent.tapEnable(tap: trackpadTapLockEvent, enable: false)
         }
+        
+        if let trackpadRunLoopSource {
+            CFRunLoopRemoveSource(CFRunLoopGetCurrent(), trackpadRunLoopSource, .commonModes)
+        }
+        
         trackpadTapLockEvent = nil
+        trackpadRunLoopSource = nil
      }
 }
--- CleanLock/Views/MacBookKeyboardView.swift
+++ CleanLock/Views/MacBookKeyboardView.swift
@@ -8,67 +8,9 @@
 import SwiftUI
 
 struct MacBookKeyboardView: View {
+    @EnvironmentObject private var inputManager: InputBlockingManager
     @State private var pressedKey: String? = nil
     @State private var trackpadPressed: Bool = false
-
-    // Layout dos teclados ANSI EUA
-    private let keyboard = [
-        [
-            (53, "esc"), (1001, "F1"), (1002, "F2"), (160, "F3"), (177, "F4"),
-            (176, "F5"), (178, "F6"),
-            (1003, "F7"), (1004, "F8"), (1005, "F9"), (1006, "F10"),
-            (1007, "F11"), (1008, "F12"), (1009, "on"),
-        ],
-
-        [
-            (50, "`"), (18, "1"), (19, "2"), (20, "3"), (21, "4"), (23, "5"),
-            (22, "6"),
-            (26, "7"), (28, "8"), (25, "9"), (29, "0"), (27, "-"), (24, "="),
-            (51, "delete"),
-        ],
-
-        [
-            (48, "tab"), (12, "Q"), (13, "W"), (14, "E"), (15, "R"), (17, "T"),
-            (16, "Y"),
-            (32, "U"), (34, "I"), (31, "O"), (35, "P"), (33, "["), (30, "]"),
-            (42, "\\"),
-        ],
-
-        [
-            (1000, "caps"), (0, "A"), (1, "S"), (2, "D"), (3, "F"), (5, "G"),
-            (4, "H"),
-            (38, "J"), (40, "K"), (37, "L"), (41, ";"), (39, "'"),
-            (36, "return"),
-        ],
-
-        [
-            (56, "shift"), (6, "Z"), (7, "X"), (8, "C"), (9, "V"), (11, "B"),
-            (45, "N"),
-            (46, "M"), (43, ","), (47, "."), (44, "/"), (60, "shift"),
-        ],
-
-        [
-            (63, "fn"), (59, "control"), (58, "option"), (55, "command"),
-            (49, ""), (54, "command"), (61, "option"),
-            (123, "←"), (125, "↑↓"), (124, "→"),
-        ],
-
-    ]
-
-    /*    private let keyboard = [["esc", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "on"],
-                            ["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "delete"],
-                            ["tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]", "\\"],
-                            ["caps", "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "return"],
-                            ["shift", "Z", "X", "C", "V", "B", "N", "M", ",", ".", "/", "shift"],
-                            ["fn", "control", "option", "command", "", "command", "option", "←", "↑↓", "→"]]*/
-
-    // Cada linha do teclado tem o seguinte tamanho em pontos para um frame da janela 600 x 600
-    // Linha 1: 13 * 20 (teclas normais) + 13 * 3 (espaços entre as teclas) + 1 * 20 * 1.6 (tecla esc)
-    // Linha 2: 13 * 20 (teclas normais) + 13 * 3 (espaços entre as teclas) + 1 * 20 * 1.6 (tecla del)
-    // Linha 3: 13 * 20 (teclas normais) + 13 * 3 (espaços entre as teclas) + 1 * 20 * 1.6 (tecla tab)
-    // Linha 4: 11 * 20 (teclas normais) + 12 * 3 (espaços entre as teclas) + 2 * 20 * 1.9 (teclas caps e enter)
-    // Linha 5: 10 * 20 (teclas normais) + 11 * 3 (espaços entre as teclas) + 2 * 20 * 2.5 (teclas shift)
-    // Linha 6:  7 * 20 (teclas normais) +  9 * 3 (espaços entre as teclas) + 2 * 20 * 1.3 (teclas command) + 1 * 20 * 5.7 (tecla espaço)
 
     // Dimensões relativas
     private var horizontalSpaceBetweenKeys: CGFloat {
@@ -107,18 +49,6 @@ struct MacBookKeyboardView: View {
         geometry.size.width / 82.475
     }
 
-    private let standardKeyWidth: CGFloat = 1.0
-    private let specialKeyWidths: [String: CGFloat] = [
-        "esc": 1.6,
-        "delete": 1.6,
-        "tab": 1.6,
-        "caps": 1.875,
-        "return": 1.875,
-        "shift": 2.45,
-        "command": 1.3,
-        "": 5.6,  // Barra de espaço
-    ]
-
     var geometry: GeometryProxy
 
     var body: some View {
@@ -138,7 +68,7 @@ struct MacBookKeyboardView: View {
                         height: keyboardBackgroundHeight)
 
                 // Conteúdo do teclado
-                keyRows(of: keyboard, containerDimension: geometry)
+                keyRows(of: inputManager.keyboard, containerDimension: geometry)
             }
 
             // Trackpad
@@ -150,11 +80,6 @@ struct MacBookKeyboardView: View {
     @ViewBuilder
     func trackpadView(containerDimension geometry: GeometryProxy) -> some View {
         Button {
-            self.trackpadPressed = true
-            // Simula clique no trackpad
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                self.trackpadPressed = false
-            }
         } label: {
             RoundedRectangle(cornerRadius: 6)
                 .fill(Color.black.opacity(0.2))
@@ -169,6 +94,14 @@ struct MacBookKeyboardView: View {
         }
         .buttonStyle(PlainButtonStyle())
     }
+    
+    // Cada linha do teclado ANSI EUA tem o seguinte tamanho em pontos para um frame da janela 600 x 600
+    // Linha 1: 13 * 20 (teclas normais) + 13 * 3 (espaços entre as teclas) + 1 * 20 * 1.6 (tecla esc)
+    // Linha 2: 13 * 20 (teclas normais) + 13 * 3 (espaços entre as teclas) + 1 * 20 * 1.6 (tecla del)
+    // Linha 3: 13 * 20 (teclas normais) + 13 * 3 (espaços entre as teclas) + 1 * 20 * 1.6 (tecla tab)
+    // Linha 4: 11 * 20 (teclas normais) + 12 * 3 (espaços entre as teclas) + 2 * 20 * 1.9 (teclas caps e enter)
+    // Linha 5: 10 * 20 (teclas normais) + 11 * 3 (espaços entre as teclas) + 2 * 20 * 2.5 (teclas shift)
+    // Linha 6:  7 * 20 (teclas normais) +  9 * 3 (espaços entre as teclas) + 2 * 20 * 1.3 (teclas command) + 1 * 20 * 5.7 (tecla espaço)
 
     @ViewBuilder
     func keyRows(of keyboard: [[(Int, String)]], containerDimension geometry: GeometryProxy) -> some View {
@@ -185,14 +118,9 @@ struct MacBookKeyboardView: View {
 
     @ViewBuilder
     func keyButton(key: String, viewDimension geometry: GeometryProxy) -> some View {
-        let keyWidthScaleFactor = specialKeyWidths[key] ?? standardKeyWidth
+        let keyWidthScaleFactor = inputManager.specialKeyWidths[key] ?? inputManager.standardKeyWidth
 
         Button {
-            self.pressedKey = key
-            // Simula o pressionamento das teclas
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                self.pressedKey = nil
-            }
         } label: {
             ZStack {
                 // Fundo da tecla com baixo relevo
@@ -213,10 +141,22 @@ struct MacBookKeyboardView: View {
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
             }
             .frame(width: keyWidthScaleFactor * keyWidth, height: keyHeight)
-            .scaleEffect(pressedKey == key ? 0.95 : 1.0)
+            .scaleEffect(pressedKey == key ? 0.9 : 1.0)
             .animation(.spring(response: 0.1), value: pressedKey == key)
         }
         .buttonStyle(PlainButtonStyle())
+        .onChange(of: inputManager.pressedKeyCode) { _, pressedKeyCode in
+            // Simula o pressionamento das teclas
+            guard let pressedKeyCode else { return }
+
+            self.pressedKey = inputManager.findKeyLabel(for: pressedKeyCode)
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                self.pressedKey = nil
+                inputManager.setPressedKeyCodeValue(nil)
+            }
+
+        }
     }
 
     // Função para retornar o rótulo apropriado para cada tecla
@@ -230,15 +170,21 @@ struct MacBookKeyboardView: View {
             return "⇪"
         case "return":
             return "↩"
-        case "shift":
+        case "shift l":
+            return "⇧"
+        case "shift r":
             return "⇧"
         case "fn":
             return "fn"
         case "control":
             return "⌃"
-        case "option":
+         case "option l":
             return "⌥"
-        case "command":
+       case "option r":
+            return "⌥"
+         case "command l":
+            return "⌘"
+       case "command r":
             return "⌘"
         case "":
             return ""  // Tecla de espaço - fica em branco
